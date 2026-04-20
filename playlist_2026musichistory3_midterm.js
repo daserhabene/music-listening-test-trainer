44601b7d9a4aa27a53d610540cca2414c520fcdb
vyurihwa <generalbrian0423@gmail.com>
2026-04-20 11:56:07 +0900

    Add files via upload

---
+++ playlist_2026musichistory3_midterm.js
@@ -0,0 +1,162 @@
+playlist = [
+  {
+    composer: "하이든",
+    title: "현악4중주 농담 4악장",
+    aliases: ["농담", "농담 4악장"],
+    file: "audio/Emerson String Quartet 및 Joseph Haydn - Haydn_ 하이든_ 현악 사중주 내림 마장조, 작품번호 33-2_ 4. Finale_ Presto.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "하이든",
+    title: "교향곡 104번 4악장",
+    aliases: ["104번", "교향곡 104번"],
+    file: "audio/J. Haydn_ Sinfonía nº 104 en Re Mayor IV - Finale_ Spiritoso.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "모차르트",
+    title: "돈 조반니 서곡",
+    aliases: ["돈 조반니"],
+    file: "audio/Don Giovanni - Ouverture (score).mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "모차르트",
+    title: "현악4중주 불협화 1악장",
+    aliases: ["불협화", "불협화 1악장"],
+    file: "audio/Wolfgang Amadeus Mozart - String Quartet No. 19 Dissonance, K. 465 [With score].mp3",
+    start: 6,
+    end: 66
+  },
+  {
+    composer: "모차르트",
+    title: "클라리넷 협주곡 2악장",
+    aliases: ["클라리넷 협주곡", "클라리넷 협주곡 2악장"],
+    file: "audio/Wolfgang Amadeus Mozart_ Clarinet Concerto in A major, K.622.mp3",
+    start: 778,
+    end: 838
+  },
+  {
+    composer: "모차르트",
+    title: "교향곡 40번 1악장",
+    aliases: ["40번", "교향곡 40번"],
+    file: "audio/Symphony No. 40 in G minor, K.550 (Mozart) - Sheet Music.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "베토벤",
+    title: "피아노 소나타 8번 비창 1악장",
+    aliases: ["비창", "비창 1악장", "피아노 소나타 8번"],
+    file: "audio/Beethoven_ Sonata No.8 in C Minor, Op.13, Pathétique (Feltsman, Lortie, Korstick).mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "베토벤",
+    title: "현악4중주 7번 라주모프스키 1악장",
+    aliases: ["라주모프스키", "라주모프스키 1악장", "현악4중주 7번"],
+    file: "audio/Beethoven_ String Quartet no. 7 in F major, op. 59 no. 1.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "베토벤",
+    title: "피아노 협주곡 5번 황제 1악장",
+    aliases: ["황제", "황제 1악장", "피아노 협주곡 5번"],
+    file: "audio/조성진│베토벤, 피아노 협주곡 5번 Op. 73 '황제' (L.v.Beethoven, Piano Concerto No.5  'Emperor') Pf. Seongjin Cho.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "베토벤",
+    title: "교향곡 3번 영웅 1악장",
+    aliases: ["영웅", "영웅 1악장", "교향곡 3번"],
+    file: "audio/Beethoven_ Symphony no. 3 in E flat major Eroica, op. 55.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "슈베르트",
+    title: "겨울나그네 보리수나무",
+    aliases: ["보리수", "보리수나무"],
+    file: "audio/Jonas Kaufmann 'Der Lindenbaum' Winterreise.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "슈만",
+    title: "시인의 사랑 아름다운 오월에",
+    aliases: ["아름다운 오월에", "시인의 사랑"],
+    file: "audio/Dichterliebe - 'Im wünderschönen Monat Mai' - Schumann.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "슈베르트",
+    title: "방랑자 환상곡 1악장",
+    aliases: ["방랑자 환상곡", "방랑자"],
+    file: "audio/Schubert_ Wanderer Fantasy in C Major, Op.15 (Lewis).mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "슈만",
+    title: "카니발 플로레스탄",
+    aliases: ["플로레스탄", "카니발"],
+    file: "audio/Schumann. Carnaval Op. 9. 6. Florestan. Partitura. Audición..mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "쇼팽",
+    title: "폴로네이즈 영웅",
+    aliases: ["영웅 폴로네이즈", "폴로네이즈"],
+    file: "audio/Chopin - Polonaise, Op. 53 (Kissin).mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "리스트",
+    title: "파가니니 주제에 의한 연습곡 중 라 캄파넬라",
+    aliases: ["라 캄파넬라", "캄파넬라"],
+    file: "audio/Liszt Paganini Etude S.161 No.3 La Campenella (Li).mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "브람스",
+    title: "인터메초",
+    aliases: ["인터메초 op 118"],
+    file: "audio/Brahms - Intermezzo in A major, Op. 118 No. 2 (Stephen Kovacevich) - 1981.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "멘델스존",
+    title: "피아노 트리오 1번 1악장",
+    aliases: ["피아노 트리오", "트리오 1번"],
+    file: "audio/Felix Mendelssohn - Piano Trio No. 1 in D minor.mp3",
+    start: 5,
+    end: 65
+  },
+  {
+    composer: "슈만",
+    title: "피아노 4중주 3악장",
+    aliases: ["피아노 4중주", "4중주 3악장"],
+    file: "audio/Schumann, Piano Quartet in Eb Major, Op. 47, III. Andante cantabile, BCMF 2014.mp3",
+    start: 0,
+    end: 60
+  },
+  {
+    composer: "브람스",
+    title: "독일 레퀴엠 1곡 '애통하는 자는 복이 있나니'",
+    aliases: ["독일 레퀴엠", "레퀴엠 1곡", "애통하는 자는 복이 있나니"],
+    file: "audio/Brahms - Ein deutsches Requiem - 1. Selig sind.mp3",
+    start: 22,
+    end: 82
+  }
+];
